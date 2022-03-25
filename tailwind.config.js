703c2a93c9d4f407c5dd1d03d447fdf060554b5e
Celso Miranda <769237+celsomiranda@users.noreply.github.com>
2022-03-25 11:28:28 +0000

    nav & footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{html,njk}"],
   theme: {
+    fontFamily: {
+      mono: ['Fira Code', 'Cascadia Code', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace']
+    },
     extend: {},
   },
   plugins: [],
